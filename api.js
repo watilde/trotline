860472ee86543ecb8ae37b00249f80ebc2625101
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-03-05 05:45:30 +0900

    [auto] Deployed to gh-pages

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Server",
+        "Trotline"
+    ],
+    "modules": [
+        "trotline"
+    ],
+    "allModules": [
+        {
+            "displayName": "trotline",
+            "name": "trotline",
+            "description": "Trotline Interface"
+        }
+    ]
+} };
+});

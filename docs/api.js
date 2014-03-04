4633d4828e43c7770eb2aebde8f473a666ed012b
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-03-05 04:55:30 +0900

    Add gulp-yuidoc

---
+++ docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "trotline"
+    ],
+    "modules": [
+        "server"
+    ],
+    "allModules": [
+        {
+            "displayName": "server",
+            "name": "server",
+            "description": "Create server"
+        }
+    ]
+} };
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var watch  = require('gulp-watch');
-var path   = './lib/*.js';
+var yuidoc = require("gulp-yuidoc");
 
-gulp.task('watch', function() {
-  gulp.watch(path, ['test']);
-});
+var path   = ['./lib/main.js', './lib/trotline/*.js'];
 
 gulp.task('test', function() {
   return gulp.src(path)
     .pipe(jshint());
 });
 
-gulp.task('default', ['test', 'watch']);
+gulp.task('docs', function() {
+  return gulp.src(path)
+    .pipe(yuidoc())
+    .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('build', ['test', 'docs']);
+
+gulp.task('watch', function() {
+  gulp.watch(path, ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
